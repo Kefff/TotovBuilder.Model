c8624bf5ed407089466b561a18472e280ef27f74
Kefff <1677411+Kefff@users.noreply.github.com>
2023-12-04 00:46:05 +0100

    Updated configuration.

--- src/TotovBuilder.Model/Configuration/AzureFunctionsConfiguration.cs
+++ src/TotovBuilder.Model/Configuration/AzureFunctionsConfiguration.cs
@@ -30,19 +30,9 @@ namespace TotovBuilder.Model.Configuration
         public string ApiPricesQuery { get; set; } = string.Empty;
 
         /// <summary>
-        /// API query for getting prices.
-        /// </summary>
-        public string ApiQuestsQuery { get; set; } = string.Empty;
-
-        /// <summary>
         /// API URL.
         /// </summary>
         public string ApiUrl { get; set; } = string.Empty;
-
-        /// <summary>
-        /// Name of the Azure blob containing the armor penetrations.
-        /// </summary>
-        public string AzureArmorPenetrationsBlobName { get; set; } = string.Empty;
 
         /// <summary>
         /// Connection string to the Azure blob storage containing static data.
@@ -50,39 +40,20 @@ namespace TotovBuilder.Model.Configuration
         public string AzureBlobStorageConnectionString { get; set; } = string.Empty;
 
         /// <summary>
-        /// Name of the Azure blob storage containing static data.
+        /// Name of the Azure blob storage containing raw data used to generate data used by the website.
         /// </summary>
-        public string AzureBlobStorageContainerName { get; set; } = string.Empty;
+        public string AzureBlobStorageRawDataContainerName { get; set; } = string.Empty;
 
         /// <summary>
-        /// Name of the Azure blob containing the changelog.
+        /// Name of the Azure blob storage containing the generated data used by the website.
         /// </summary>
-        public string AzureChangelogBlobName { get; set; } = string.Empty;
+        public string AzureBlobStorageWebsiteDataContainerName { get; set; } = string.Empty;
 
         /// <summary>
-        /// Name of the Azure blob containing the Azure Functions configuration.
+        /// Name of the Azure blob containing Azure Functions configuration data.
+        /// Stored in the same container as raw data.
         /// </summary>
         public string AzureFunctionsConfigurationBlobName { get; set; } = string.Empty;
-
-        /// <summary>
-        /// Name of the Azure blob containing the item categories.
-        /// </summary>
-        public string AzureItemCategoriesBlobName { get; set; } = string.Empty;
-
-        /// <summary>
-        /// Name of the Azure blob containing items data missing from the API.
-        /// </summary>
-        public string AzureItemMissingPropertiesBlobName { get; set; } = string.Empty;
-
-        /// <summary>
-        /// Name of the Azure blob containing the values related to Tarkov gameplay.
-        /// </summary>
-        public string AzureTarkovValuesBlobName { get; set; } = string.Empty;
-
-        /// <summary>
-        /// Name of the Azure blob containing the website configuration.
-        /// </summary>
-        public string AzureWebsiteConfigurationBlobName { get; set; } = string.Empty;
 
         /// <summary>
         /// Time (in seconds) fetched API data is kept before needing to refresh it.
@@ -100,17 +71,68 @@ namespace TotovBuilder.Model.Configuration
         public int PriceCacheDuration { get; set; } = 3600;
 
         /// <summary>
-        /// Gets the names of the blobs to upload to Azure.
+        /// Name of the Azure blob containing raw armor penetrations data.
         /// </summary>
-        /// <returns>Names of the blobs to upload.</returns>
-        public IEnumerable<string> GetBlobNames()
-        {
-            Type azureFunctionsConfigurationType = typeof(AzureFunctionsConfiguration);
-            IEnumerable<string> blobsToUpload = azureFunctionsConfigurationType.GetProperties()
-                .Where(p => p.Name.EndsWith("BlobName"))
-                .Select(p => p.GetValue(this) as string ?? string.Empty);
+        public string RawArmorPenetrationsBlobName { get; set; } = string.Empty;
 
-            return blobsToUpload;
-        }
+        /// <summary>
+        /// Name of the Azure blob containing raw changelog data.
+        /// </summary>
+        public string RawChangelogBlobName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Name of the Azure blob containing raw item categories data.
+        /// </summary>
+        public string RawItemCategoriesBlobName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Name of the Azure blob containing raw items data missing from the API.
+        /// </summary>
+        public string RawItemMissingPropertiesBlobName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Name of the Azure blob containing raw data related to Tarkov gameplay.
+        /// </summary>
+        public string RawTarkovValuesBlobName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Name of the Azure blob containing raw website configuration data.
+        /// </summary>
+        public string RawWebsiteConfigurationBlobName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Name of the Azure blob containing changelog data for the website.
+        /// </summary>
+        public string WebsiteChangelogBlobName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Name of the Azure blob containing item categories data for the website.
+        /// </summary>
+        public string WebsiteItemCategoriesBlobName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Name of the Azure blob containing items data for the website.
+        /// </summary>
+        public string WebsiteItemsBlobName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Name of the Azure blob containing presets data for the website.
+        /// </summary>
+        public string WebsitePresetsBlobName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Name of the Azure blob containing prices data for the website.
+        /// </summary>
+        public string WebsitePricesBlobName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Name of the Azure blob containing data related to Tarkov gameplay for the website.
+        /// </summary>
+        public string WebsiteTarkovValuesBlobName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Name of the Azure blob containing website configuration data for the website.
+        /// </summary>
+        public string WebsiteWebsiteConfigurationBlobName { get; set; } = string.Empty;
     }
 }
